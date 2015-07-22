caf284d3b73f653a6377961b557a3aa7f433b1bb
Tim Jaroch <jarocht@mail.gvsu.edu>
2015-07-22 12:09:57 -0400

    Updating Storyboard layouts and adding views

---
+++ AlarmDJ/AlarmDJ/MusicSelectorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MusicSelectorViewController.swift
+//  AlarmDJ
+//
+//  Created by X Code User on 7/22/15.
+//  Copyright (c) 2015 Tim Jaroch, Morgan Heyboer, Andreas Plüss (TEAM E). All rights reserved.
+//
+
+import UIKit
+
+class MusicSelectorViewController: UIViewController {
+    
+}
